d509ba6b0214a9dbf5c057ed3f50fbe2fff74f1c
tetra-mix <ryouma1128@outlook.jp>
2024-09-09 01:14:39 +0900

    fix: Change to CSR

--- src/app/Article/[id]/page.tsx
+++ src/app/Article/[id]/page.tsx
@@ -1,42 +1,52 @@
-'use client'
-
 import axios from "axios";
-import React, { useEffect, useState } from 'react';
+import React, { use } from 'react';
 import { Button, Center } from "@yamada-ui/react";
-import { useParams } from "next/navigation";
 import Link from "next/link";
 import { ArticlesProps } from '@/components/Article';
 import { PageTemplate } from "@/components/PageTemplate";
 import { Title } from "@/components/Title";
 import parse from "html-react-parser";
 
-const Article: React.FC = () => {
-    const [posts, setPosts] = useState<ArticlesProps>({ id: "", title: "", date: "", body: "" });
-    const param = useParams();
+export const generateStaticParams = async () => {
+    const res = await axios.get('https://blog-test-dev.microcms.io/api/v1/articles', {
+        headers: {
+            'X-MICROCMS-API-KEY': '2YBb9n1AIKYI0Ltk2AzmPh0bZtPRDkkCG1d1'
+        }
+    });
 
-    useEffect(() => {
-        const id = param.id;
-        const fetchPosts = async () => {
-            const res = await axios.get('https://blog-test-dev.microcms.io/api/v1/articles/' + id, {
-                headers: {
-                    'X-MICROCMS-API-KEY': '2YBb9n1AIKYI0Ltk2AzmPh0bZtPRDkkCG1d1'
-                }
-            });
-            setPosts(res.data);
-            console.log(res.data);
-        };
+    const articles:ArticlesProps[] = res.data.contents;
+    
+    return articles.map((article: { id: string }) => ({
+        id: article.id,
+    }));
+}
+async function fetchArticle(id: string) {
+    const res = await axios.get(`https://blog-test-dev.microcms.io/api/v1/articles/${id}`, {
+        headers: {
+            'X-MICROCMS-API-KEY': '2YBb9n1AIKYI0Ltk2AzmPh0bZtPRDkkCG1d1'
+        }
+    });
+    const post:ArticlesProps = res.data;
+    return post;
+}
 
-        fetchPosts();
-    }, []);
+interface ArticlePageProps {
+    params: { id: string };
+}
+
+export default function Article({ params }: ArticlePageProps) {
+    const { id } = params;
+    const article = use(fetchArticle(id));
+
 
     return (
         <PageTemplate title={"Article"}>
             <Center>
-                <Title title={posts.title} text={posts.date} />
+                <Title title={article.title} text={article.date} />
             </Center>
             <Center>
                 <div className="parse">
-                    {parse(posts.body)}
+                    {parse(article.body)}
                 </div>
             </Center>
             <Center p={16}>
@@ -49,5 +59,3 @@ const Article: React.FC = () => {
         </PageTemplate>
     );
 };
-
-export default Article;
