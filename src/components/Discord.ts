712a6dfb4cf5ec30fca45f7a12563d15279b293d
tetra-mix <ryouma1128@outlook.jp>
2024-11-06 18:19:38 +0900

    update: react-hook-from

--- src/components/Discord.ts
+++ src/components/Discord.ts
@@ -4,7 +4,7 @@ const SendDiscordMessage = async (props: EmailProps) => {
     const url = process.env.NEXT_PUBLIC_DISCORD_WEBHOOK_URL || "";
     console.log(url);
     const data = {
-        content: "## お問い合わせがありました。\n お名前:" + props.name + "\n メールアドレス:" + props.email + "\n 件名:" + props.title + "\n 内容:" + props.message + "\n"
+        content: "## お問い合わせがありました。\n お名前:" + props.name + "\n メールアドレス:" + props.email + "\n 件名:" + props.title + "\n 内容: \n" + props.message + "\n"
     };
     await fetch(url, {
         method: 'POST',
