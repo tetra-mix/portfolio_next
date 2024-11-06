cca84332bfd10226e59288c0331b716b7399a675
tetra-mix <ryouma1128@outlook.jp>
2024-11-06 15:39:40 +0900

    add: package next-image-export-optimizer

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,8 @@ const nextConfig = {
     output: 'export',
     trailingSlash: true,
     basePath: '/portfolio', // Sakura Server
+    images: { loader: "custom" },
+    transpilePackages: ["next-image-export-optimizer"],
 };
 
 export default nextConfig;
