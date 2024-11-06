cdbb64df8e0febda72eb26f6ad05f42452e68b64
tetra-mix <ryouma1128@outlook.jp>
2024-11-06 17:44:02 +0900

    update: comp connection form

--- emails/toclient.tsx
+++ emails/toclient.tsx
@@ -5,7 +5,6 @@ import { EmailProps } from "./type";
 const ClientEmail = (props: EmailProps) => {
   return (
     <Html lang="jp" dir="ltr">
-      <Heading as="h1" className="text-center">お問い合わせ内容の確認</Heading>
       <Section>
         <Text>{props.name} 様</Text>
         <Text>お問い合わせくださりありがとうございます。</Text>
@@ -14,7 +13,9 @@ const ClientEmail = (props: EmailProps) => {
       </Section>
       <Hr className="m-[16px] border-t-2 border-[#cccccc]" />
       <Heading as="h2">お問い合わせ内容：</Heading>
-      <Heading as="h2">{props.title}</Heading>
+      <Section>
+        件名：{props.title}
+      </Section>
       <Section>
         {props.message}
       </Section>
@@ -22,6 +23,10 @@ const ClientEmail = (props: EmailProps) => {
       <Section>
         <Text>お問い合わせ内容に関して修正等がある場合はこちらのメールに返信してください。</Text>
         <Text>返信には時間がかかる場合があります。あらかじめご了承ください。</Text>
+      </Section>
+      <Hr className="m-[16px] border-t-2 border-[#cccccc]" />
+      <Section>
+        <Text>clowntetra.jp 管理者 吉髙 僚眞</Text>
       </Section>
     </Html>
   );
