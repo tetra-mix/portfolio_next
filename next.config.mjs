cf7385515b7e779d390c946b828a4f2f8692d2b2
tetra-mix <ryouma1128@outlook.jp>
2024-11-30 09:33:48 +0900

    fix: profile contact link fix

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,7 @@ const nextConfig = {
     output: 'export',
     trailingSlash: true,
     images: { loader: "custom" },
-    transpilePackages: ["next-image-export-optimizer"],
-    basePath: '/portfolio', // Sakura Server
+    transpilePackages: ["next-image-export-optimizer"]
 };
 
 export default nextConfig;
